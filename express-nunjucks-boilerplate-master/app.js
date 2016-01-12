0c0cb07d19ccce0132aff5b9cec23f90677db50e
trycrmr <trycrmr@gmail.com>
2016-01-12 18:57:16 -0500

    refactors the views under an app directory. Using Rails 4.2.5 directory structure as a guide.

--- express-nunjucks-boilerplate-master/app.js
+++ express-nunjucks-boilerplate-master/app.js
@@ -4,30 +4,31 @@ var nunjucks = require('nunjucks');
 var app = express();
 
 // Setup nunjucks templating engine
-nunjucks.configure('views', {
-    autoescape: true,
-    express: app
+nunjucks.configure('app/views', {
+  autoescape: true,
+  express: app
 });
 
 app.set('port', process.env.PORT || 3000);
 
+//Routing
 // Home page
 app.get('/', function(req, res) {
-    res.render('index.html', {
-        page: 'home',
-        port: app.get('port')
-    });
+  res.render('index.html', {
+    page: 'home',
+    port: app.get('port')
+  });
 });
 
 // Other example
 app.get('/example', function(req, res) {
-    res.render('example.html', {
-        page: 'example',
-        port: app.get('port')
-    });
+  res.render('example.html', {
+    page: 'example',
+    port: app.get('port')
+  });
 });
 
 // Kick start our server
 app.listen(app.get('port'), function() {
-    console.log('Server started on port', app.get('port'));
+  console.log('Server started on port', app.get('port'));
 });
